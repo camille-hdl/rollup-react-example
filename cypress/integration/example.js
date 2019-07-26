54006573bce8f1fff800cf1019e669870d788f36
Camille Hodoul <chodoul@1egal2.com>
2019-07-26 18:03:10 +0200

    * update dependencies * use SystemJS 4 * prevent Electron from failing cypress tests when it throws on `import()`

--- cypress/integration/example.js
+++ cypress/integration/example.js
@@ -1,17 +1,29 @@
 /* eslint-disable */
 
-describe('React Rollup example', function () {
-   
-    it("Clicks and loads", function () {
-        
-        cy.visit('/');
-        cy.get("h1").should("contain", "React rollup example");
-        cy.get("p").should("contain", "Hello World");
-
-        cy.get("[data-cy=btn-load-component").click().then(() => {
-            cy.get("[data-cy=btn-load-component").should("not.exist");
-            cy.get("[data-cy=dynamic-component]").should("be.visible");
+describe("React Rollup example", function() {
+  beforeEach(() => {
+    if (window.navigator && navigator.serviceWorker) {
+      navigator.serviceWorker.getRegistrations().then(registrations => {
+        registrations.forEach(registration => {
+          registration.unregister();
         });
-    });
+      });
+    }
+  });
+  it("Clicks and loads", function() {
+    cy.on('uncaught:exception', (err, runnable) => {
+        // prevents electron from failling the test when `import()` throws
+        return false;
+    })
+    cy.visit("/");
+    cy.get("h1").should("contain", "React rollup example");
+    cy.get("p").should("contain", "Hello World");
 
+    cy.get("[data-cy=btn-load-component")
+      .click()
+      .then(() => {
+        cy.get("[data-cy=btn-load-component").should("not.exist");
+        cy.get("[data-cy=dynamic-component]").should("be.visible");
+      });
+  });
 });
